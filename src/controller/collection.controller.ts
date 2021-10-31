7ab661915d187b2813b31adf0830f5d849f9064d
Mathieu BILLY <mathieu.billy@epita.fr>
2021-10-31 21:53:42 +0100

    Added comments

--- src/controller/collection.controller.ts
+++ src/controller/collection.controller.ts
@@ -11,23 +11,27 @@ export class CollectionController {
 constructor(private collectionService: CollectionService) {}
 
   @Get()
+  // Return every collection
   findAll() : Promise<Collection[]>{
     return this.collectionService.findAll();
   }
 
   @Post()
+  // Create a new collection  
   createCollection(@Body() createCollectionDto: CreateCollectionDto) : Promise<Collection> {
       return this.collectionService.createCollection(createCollectionDto);
   }
   
   @ApiParam({name: 'name', required: true, description: 'Collection\'s name'})
   @Get('/:name/rating')
+  // Get the average rating of a collection  
   getRating(@Param() params)
   {
       return this.collectionService.computeNftRating(params.name);
   }
 
   @Get('/bestseller')
+  // Get the best selling collection
   getbestseller() : Promise<Collection> {
     return this.collectionService.findBestSeller();
   }
--- src/controller/nft.controller.ts
+++ src/controller/nft.controller.ts
@@ -14,46 +14,60 @@ export class NftController {
 constructor(private nftService: NftService) {}
 
   @Post()
+  // Create a new nft 
   createNft(@Body() createNftDto : CreateNftDto) : Promise<Nft>{
       return this.nftService.register(createNftDto);
   }
 
   @Get()
+  // Get every existing nft 
   findall(): Promise<Nft[]>{
     return this.nftService.findAll();
   }
 
+
   @Put('/owner')
+  // Add owner to a nft 
   addOwner(@Body() updateNftDto : UpdateNftDto) : Promise<Nft>
   {
       return this.nftService.updateOwners(updateNftDto);
   }
 
+  /*
+    id : Nft's ID
+    owner : name of the new owner
+  */
+  
   @Put('/sell')
+  // Sell a nft with :id as his id to :owner
   sellNft(@Body() sellNftDto: SellNftDto) : Promise<Nft>
   {
     return this.nftService.sellNft(sellNftDto);
   }
 
   @Put('/rating')
+  // Add a rating to a nft
   addRating(@Body() updateRatingDto : UpdateRatingDto) : Promise<Nft>
   {
       return this.nftService.addRating(updateRatingDto);
   }
 
   @Get('/mostrated')
+  // Find the best rated nft  
   findMostRated(): Promise<Nft>{
     return this.nftService.findMostRated();
   }
 
   @ApiParam({name: 'number', required: true, description: 'Last number sells'})
   @Get('/last/:number')
+  // Get X last sells
   getLastSells(@Param() params) : Promise<Nft[]>{
       return this.nftService.getLastSells(params.number);
   }
 
   @ApiParam({name: 'owner', required: true, description: 'Owner name'})
   @Get('/:owner')
+  //Get every nfts of a owner
   getOwner(@Param() params) : Promise<Nft[]>{
       return this.nftService.getOwner(params.owner);
   }
